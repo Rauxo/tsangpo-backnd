e7f2ab531babac5f2131e6dc3e459e0e44dd8b31
Rauxo <rahulsaikia682@gmail.com>
2025-12-24 09:57:12 +0530

    welcome tot he backend

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+
+// Connect to the database
+// connectDb();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
